1ff63af931e2d35dd2875bd1895f991789149f0c
Kerley <kerley_sousa@live.com>
2020-05-31 19:03:08 -0300

    Adicionando elementos na tela

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,39 @@ main(){
 
 class PerguntaApp extends StatelessWidget{
 
+ 
+  final List<String> perguntas = [
+    'Qual a sua cor favorita?',
+    'Qual seu animal favorito?'
+  ];
+
+  void responder(){
+    print("Resposta");
+  }
+
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Text("Olá mundo!"),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text("Resposta"),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta"),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta"),
+              onPressed: responder,
+            )
+          ],
+        ),
       )
     );
   }
